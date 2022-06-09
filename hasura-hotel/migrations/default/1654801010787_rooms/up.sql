e96a2a6a60d46d34015f65ba20cf5a77670c0ad2
DemonHa <hh360284@gmail.com>
2022-06-09 19:31:18 +0000

    Adding relationships to te tables

--- hasura-hotel/migrations/default/1654801010787_rooms/up.sql
+++ hasura-hotel/migrations/default/1654801010787_rooms/up.sql
@@ -6,3 +6,10 @@ CREATE TABLE IF NOT EXISTS "public"."rooms" (
   "created_at" TIMESTAMPTZ DEFAULT NOW() NOT NULL,
   CONSTRAINT "rooms_pkey" PRIMARY KEY ("id")
 );
+
+ALTER TABLE ONLY "public"."rooms"
+ADD CONSTRAINT "rooms_hotel_id_fkey"
+FOREIGN KEY (hotel_id) REFERENCES hotels(id)
+ON UPDATE CASCADE
+ON DELETE CASCADE
+NOT DEFERRABLE;
--- hasura-hotel/migrations/default/1654801069843_reservations/up.sql
+++ hasura-hotel/migrations/default/1654801069843_reservations/up.sql
@@ -7,3 +7,27 @@ CREATE TABLE IF NOT EXISTS "public"."reservations" (
   "created_at" TIMESTAMPTZ DEFAULT NOW() NOT NULL,
   CONSTRAINT "reservations_pkey" PRIMARY KEY ("id")
 );
+
+
+ALTER TABLE ONLY "public"."reservations"
+ADD CONSTRAINT "reservations_hotel_id_fkey"
+FOREIGN KEY (hotel_id) REFERENCES hotels(id)
+ON UPDATE CASCADE
+ON DELETE CASCADE
+NOT DEFERRABLE;
+
+
+ALTER TABLE ONLY "public"."reservations"
+ADD CONSTRAINT "reservations_room_id_fkey"
+FOREIGN KEY (room_id) REFERENCES rooms(id)
+ON UPDATE CASCADE
+ON DELETE CASCADE
+NOT DEFERRABLE;
+
+
+ALTER TABLE ONLY "public"."reservations"
+ADD CONSTRAINT "reservations_costumer_id_fkey"
+FOREIGN KEY (costumer_id) REFERENCES costumers(id)
+ON UPDATE CASCADE
+ON DELETE CASCADE
+NOT DEFERRABLE;
